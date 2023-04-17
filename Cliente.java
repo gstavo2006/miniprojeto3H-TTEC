07025ecdec65fa32f5d06775beeba544585fde3a
Gustavo Machado dos Santos <rm83051@estudante.fieb.edu.br>
2023-04-17 08:04:38 -0300

    classe java Cliente criada

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+[07:52] Adriano Domingues
+    public class Cliente {​​​​​​​
+    // Atributo
+    private String nome;
+    //Método
+    public void falar(){​​​​​​​
+        System.out.print("falei");
+    }​​​​​​​
+    // Getter e Setter
+    public String getNome(){​​​​​​​
+        return this.nome;
+    }​​​​​​​
+    public void setNome(String _nome){​​​​​​​
+        this.nome = _nome;
+    }​​​​​​​
+}​​​​​​​
